6c5892b982904bbbe681b7e92551f8acbab38787
Mojasagwe <78989798+Mojasagwe@users.noreply.github.com>
2022-01-27 10:33:03 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { About } from "./components/About";
+import Home from "./components/Home";
+import { SoftSkills } from "./components/SoftSkills";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<Home />
+			<About />
+			<SoftSkills />
+			<Skills />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
